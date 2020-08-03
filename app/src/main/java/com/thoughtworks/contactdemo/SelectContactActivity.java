fff6812204ebe3025d12916c3d11fb8e5f6573f7
zeqiang.chen <1209120868@qq.com>
2020-08-03 16:08:26 +0800

    [zeqiang] feat: add SelectContactActivity

---
+++ app/src/main/java/com/thoughtworks/contactdemo/SelectContactActivity.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.contactdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_contact);
+    }
+}
